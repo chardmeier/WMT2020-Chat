e36745ebcd5f6741b9561115b1ed4ef0084649c4
Christian Hardmeier <christian.hardmeier@lingfil.uu.se>
2020-04-23 14:19:42 +0100

    Process input in batches.

--- semstruct/create_embeddings.py
+++ semstruct/create_embeddings.py
@@ -18,6 +18,15 @@ def scan_and_compress_nbest(it):
             yield k, cand
 
 
+def make_batches(iterable, batchsize):
+    it = iter(iterable)
+    while True:
+        batch = list(itertools.islice(it, batchsize))
+        if len(batch) == 0:
+            return
+        yield batch
+
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('inputfile', help='Input text file.')
@@ -25,6 +34,7 @@ def main():
     parser.add_argument('-model', default='xlm-roberta-base', help='Model to create embeddings with.')
     parser.add_argument('-maxsent', type=int, help='Maximum number of sentences to process.')
     parser.add_argument('-nbest', action='store_true', help='Input is in MT n-best format.')
+    parser.add_argument('-batchsize', type=int, help='Batch size to process at one time.')
     args = parser.parse_args()
 
     tokeniser = transformers.AutoTokenizer.from_pretrained(args.model)
@@ -39,21 +49,29 @@ def main():
         maxsent = args.maxsent if args.maxsent else sys.maxsize
         input_lines = [(i, line) for i, line in itertools.takewhile(lambda tup: tup[0] < maxsent, line_gen)]
 
+    all_indices = []
+    all_embeddings = []
     with torch.no_grad():
-        inputs = tokeniser.batch_encode_plus((line for i, line in input_lines), pad_to_max_length=True,
-                                             return_tensors='pt', return_attention_masks=True)
-        outputs = model(inputs['input_ids'], attention_mask=inputs['attention_mask'])
+        for batch in make_batches(input_lines, args.batchsize):
+            inputs = tokeniser.batch_encode_plus((line for i, line in batch), pad_to_max_length=True,
+                                                 return_tensors='pt', return_attention_masks=True)
+            outputs = model(inputs['input_ids'], attention_mask=inputs['attention_mask'])
 
-    word_embeddings = outputs[0]
-    batchsize, _, embsize = word_embeddings.shape
-    sentence_indices = torch.LongTensor([i for i, line in input_lines])
-    sentence_embeddings = torch.empty(batchsize, embsize)
-    for i in range(batchsize):
-        snt_we = word_embeddings[i, inputs['attention_mask'][i].bool(), :]
-        sentence_embeddings[i, :] = torch.mean(snt_we, dim=0)
+            word_embeddings = outputs[0]
+            batchsize, _, embsize = word_embeddings.shape
+            sentence_indices = torch.LongTensor([i for i, line in input_lines])
+            sentence_embeddings = torch.empty(batchsize, embsize)
+            for i in range(batchsize):
+                snt_we = word_embeddings[i, inputs['attention_mask'][i].bool(), :]
+                sentence_embeddings[i, :] = torch.mean(snt_we, dim=0)
+            all_indices.append(sentence_indices)
+            all_embeddings.append(sentence_embeddings)
+
+    all_indices = torch.cat(all_indices)
+    all_embeddings = torch.cat(all_embeddings)
 
     with open(args.outputfile, 'w') as f:
-        torch.save([sentence_indices, sentence_embeddings], f)
+        torch.save([all_indices, all_embeddings], f)
 
 
 if __name__ == '__main__':
