31d3de92ee22eca17a55cfa014e24dd9d84d0259
Christian Hardmeier <christian.hardmeier@lingfil.uu.se>
2020-04-23 11:41:28 +0100

    Read n-best lists.

--- semstruct/create_embeddings.py
+++ semstruct/create_embeddings.py
@@ -1,7 +1,15 @@
 import argparse
 import itertools
+import sys
 import torch
 import transformers
+
+
+def scan_nbest(it):
+    for line in it:
+        fields = line.split(' ||| ')
+        if len(fields) >= 2:
+            yield int(fields[0]), fields[1]
 
 
 def main():
@@ -10,19 +18,27 @@ def main():
     parser.add_argument('outputfile', help='File to save embeddings to.')
     parser.add_argument('-model', default='xlm-roberta-base', help='Model to create embeddings with.')
     parser.add_argument('-maxsent', type=int, help='Maximum number of sentences to process.')
+    parser.add_argument('-nbest', action='store_true', help='Input is in MT n-best format.')
     args = parser.parse_args()
 
     tokeniser = transformers.AutoTokenizer.from_pretrained(args.model)
     model = transformers.AutoModel.from_pretrained(args.model).eval()
 
     with open(args.inputfile, 'r') as f:
-        input_lines = [line.rstrip('\n') for line in itertools.islice(f, args.maxsent)]
+        if args.nbest:
+            line_gen = scan_nbest(f)
+        else:
+            line_gen = enumerate(line.rstrip('\n') for line in f)
 
-    inputs = tokeniser.batch_encode_plus(input_lines, return_tensors='pt')
+        maxsent = args.maxsent if args.maxsent else sys.maxsize
+        input_lines = [(i, line) for i, line in itertools.takewhile(lambda i, line: i < maxsent, line_gen)]
+
+    inputs = tokeniser.batch_encode_plus((line for i, line in input_lines), return_tensors='pt')
     outputs = model(inputs['input_ids'], attention_mask=inputs['attention_mask'])
 
     word_embeddings = outputs[0]
     batchsize, _, embsize = word_embeddings.shape
+    sentence_indices = torch.LongTensor([i for i, line in input_lines])
     sentence_embeddings = torch.empty(batchsize, embsize)
     for i in range(batchsize):
         sentence_embeddings[i, :] = torch.mean(torch.masked_select(word_embeddings[i],
@@ -30,7 +46,7 @@ def main():
                                                dim=0)
 
     with open(args.outputfile, 'w') as f:
-        torch.save(sentence_embeddings, f)
+        torch.save([sentence_indices, sentence_embeddings], f)
 
 
 if __name__ == '__main__':
