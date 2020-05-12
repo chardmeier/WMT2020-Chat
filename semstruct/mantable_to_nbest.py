f6c832bcfbebe4ecb7f001d89ac831ca651e43ae
Christian Hardmeier <christian.hardmeier@lingfil.uu.se>
2020-05-12 16:36:32 +0100

    Added mantable_to_nbest.py.

---
+++ semstruct/mantable_to_nbest.py
@@ -0,0 +1,40 @@
+import argparse
+import pandas
+import sys
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('table', help='Manual evaluation spreadsheet (TSV).')
+    args = parser.parse_args()
+
+    with open(args.table, 'r') as f:
+        table = pandas.read_table(f, header=0)
+
+    ref_col = 2
+    first_sys = 6
+
+    for i, snt in table.iterrows():
+        if snt['Speaker'] != 'agent':
+            continue
+
+        pos = [ref_col] + list(range(first_sys, len(snt), 2))
+        cands = set((snt.iloc[j], snt.iloc[j + 1]) for j in pos if not pandas.isna(snt.iloc[j + 1]))
+
+        seen = set()
+        discard = set()
+        for t, c in cands:
+            if t in seen:
+                discard.add(t)
+            seen.add(t)
+        if discard:
+            print('Discarding conflicting annotations in sentence', i, file=sys.stderr)
+            cands = [(t, c) for t, c in cands if c not in discard]
+
+        if len(cands) > 1:
+            for t, c in cands:
+                print('%d ||| %s ||| %d' % (i, t, c))
+
+
+if __name__ == '__main__':
+    main()
